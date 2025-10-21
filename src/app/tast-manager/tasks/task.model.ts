731f6f171dd92cb744c2537e17f04b60880b6b98
firas1996 <firas.hm.1996@gmail.com>
2025-10-21 09:45:24 +0100

    aa

---
+++ src/app/tast-manager/tasks/task.model.ts
@@ -0,0 +1,13 @@
+export interface TheTask {
+  id: number;
+  userId: string;
+  title: string;
+  description: string;
+  deadline: string;
+}
+
+export interface TaskData {
+  title: string;
+  description: string;
+  deadline: string;
+}
--- src/app/tast-manager/tasks/tasks.ts
+++ src/app/tast-manager/tasks/tasks.ts
@@ -1,11 +1,65 @@
 import { Component, input } from '@angular/core';
+import { Task } from './task/task';
 
 @Component({
   selector: 'app-tasks',
-  imports: [],
+  imports: [Task],
   templateUrl: './tasks.html',
   styleUrl: './tasks.css',
 })
 export class Tasks {
   userName = input<string>();
+
+  userId = input<string>();
+
+  tasksData = [
+    {
+      id: 1,
+      userId: 'u3',
+      title: 'Task1',
+      description: 'this is a task description',
+      deadline: 'Feb 21th at 8:30am',
+    },
+    {
+      id: 2,
+      userId: 'u1',
+      title: 'Task2',
+      description: 'this is a task description',
+      deadline: 'Feb 21th at 8:30am',
+    },
+    {
+      id: 3,
+      userId: 'u5',
+      title: 'Task3',
+      description: 'this is a task description',
+      deadline: 'Feb 21th at 8:30am',
+    },
+    {
+      id: 4,
+      userId: 'u2',
+      title: 'Task4',
+      description: 'this is a task description',
+      deadline: 'Feb 21th at 8:30am',
+    },
+    {
+      id: 5,
+      userId: 'u2',
+      title: 'Task5',
+      description: 'this is a task description',
+      deadline: 'Feb 21th at 8:30am',
+    },
+    {
+      id: 6,
+      userId: 'u3',
+      title: 'Task6',
+      description: 'this is a task description',
+      deadline: 'Feb 21th at 8:30am',
+    },
+  ];
+
+  get selectedUserTasks() {
+    return this.tasksData.filter((task) => {
+      return task.userId === this.userId();
+    });
+  }
 }
